814afd3f454ceef16a29a62377a2f5f69652cf72
ankurjay <ankur.jay007@gmail.com>
2026-05-03 15:12:29 +0400

    Add TGAImage Library
    1) Update the folder structure to separate headers from library components
    2) Update meson.build to reflect updated folder structure
    3) Update main.cpp to reflect basic image generation using the TGAImage class

--- nanoRenderer/src/main.cpp
+++ nanoRenderer/src/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "tgaimage.h"
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+int main(int argc, char** argv) {
+    constexpr int width  = 64;
+    constexpr int height = 64;
+
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+    framebuffer.write_tga_file("framebuffer.tga");
+
     return 0;
 }
