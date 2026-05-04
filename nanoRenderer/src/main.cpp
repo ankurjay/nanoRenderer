fde2b95de81f7a96c1f803b6253d6c7cc9cbdc35
ankurjay <ankur.jay007@gmail.com>
2026-05-04 12:56:59 +0400

    Add Line Drawing Algorithm v1

--- nanoRenderer/src/main.cpp
+++ nanoRenderer/src/main.cpp
@@ -1,20 +1,21 @@
+#include <cmath>
 #include "tgaimage.h"
 
+constexpr TGAColor white = {255, 255, 255, 255};
+constexpr TGAColor green = {0, 255, 0, 255};
+constexpr TGAColor red = {0, 0, 255, 255};
+constexpr TGAColor blue = {255, 128, 64, 255};
+constexpr TGAColor yellow = {0, 200, 255, 255};
 
-/**
-* @brief Basic function that takes in a TGAImage and sets 3 pixels to white at specified coordinates
-*/
-void example(TGAImage &framebuffer) {
-    constexpr TGAColor white = {255, 255, 255, 255 };
-    
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
+{
+    for (float t = 0.f; t < 1.f; t += 0.02f) {
+        int x = std::round( ax + (bx - ax) * t );
+        int y = std::round( ay + (by - ay) * t );
+        framebuffer.set(x, y, color);
+    }
 }
+
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
@@ -23,8 +24,22 @@ int main(int argc, char** argv) {
     // Create a blank buffer
     TGAImage framebuffer(width, height, TGAImage::RGB);
     
-    // Modify pixels
-    example(framebuffer);
+    // Define points
+    int ax = 7, ay = 3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+    
+    // Draw Lines
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    // same line as above, but coordinates reversed
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    // Draw corner points
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
 
     // Write the buffer to a file
     framebuffer.write_tga_file("framebuffer.tga");
